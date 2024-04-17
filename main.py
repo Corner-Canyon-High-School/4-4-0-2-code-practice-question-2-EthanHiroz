e5a847f5fd9aad3ac75ec99372db5deeb3482e12
EthanHiroz <162729379+EthanHiroz@users.noreply.github.com>
2024-04-17 16:27:51 +0000

    Submit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+red = int(input("Enter the Red: "))
+green = int(input("Enter the Green: "))
+blue = int(input("Enter the Blue: "))
+
+
+if (red < 0 or red > 255):
+    print("Red number is not correct")
+    if (green < 0 or green > 255):
+        print("Green number is not correct")
+        if (blue < 0 or blue > 255):
+            print("Blue number is not correct")
+else:
+    print("No problems found!")
+
+
 
